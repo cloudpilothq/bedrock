4c74f9b36f94f27616d7f3ec2a142fad71672918
Victor Omerusure Umukoro <appvictorben@gmail.com>
2026-06-04 00:32:34 +0100

    feat: phase 2 database layer and iam

---
+++ terraform/iam.tf
@@ -0,0 +1,18 @@
+# Create the IAM user for developers
+resource "aws_iam_user" "dev_view" {
+  name = "bedrock-dev-view"
+}
+
+# Attach a basic managed policy (optional, but good for base AWS console access)
+resource "aws_iam_user_policy_attachment" "dev_view_readonly" {
+  user       = aws_iam_user.dev_view.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+# EKS Access Entry to grant Kubernetes cluster access to the IAM user
+resource "aws_eks_access_entry" "dev_view_entry" {
+  cluster_name      = module.eks.cluster_name
+  principal_arn     = aws_iam_user.dev_view.arn
+  kubernetes_groups = ["system:masters"] # For lab simplicity. In prod, use a restricted RBAC group!
+  type              = "STANDARD"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,13 @@ output "vpc_id" {
   description = "VPC ID"
   value       = module.vpc.vpc_id
 }
+
+output "assets_bucket_name" {
+  description = "Name of the S3 bucket for assets"
+  value       = aws_s3_bucket.assets.id
+}
+
+output "dev_view_iam_user_arn" {
+  description = "ARN of the dev-view IAM user"
+  value       = aws_iam_user.dev_view.arn
+}
