11671553adb90ac53227e9d6556ff464d3faa586
Victor Ome <appvictorben@gmail.com>
2026-06-06 09:15:40 +0100

    fix: add EKS addon IAM role with required policies

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,37 @@
+# Create IAM role for EKS addons
+resource "aws_iam_role" "eks_addon_role" {
+  name = "bedrock-eks-addon-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Project = "karatu-2025-capstone"
+  }
+}
+
+# Attach policies for EKS addons
+resource "aws_iam_role_policy_attachment" "eks_addon_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+  ])
+
+  role       = aws_iam_role.eks_addon_role.name
+  policy_arn = each.value
+}
+
 # Create IAM user
 resource "aws_iam_user" "dev_view" {
   name = "bedrock-dev-view"
