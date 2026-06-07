e861a40261f52ad36711b42700bbcc6d63d70065
Victor Omerusure Umukoro <appvictorben@gmail.com>
2026-06-07 17:02:18 +0100

    Remove duplicate kubernetes provider block

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -42,18 +42,7 @@ resource "aws_eks_access_entry" "dev_view" {
   type              = "STANDARD"
 }
 
-# Provider needed for Kubernetes resources
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
-}
 
 resource "kubernetes_cluster_role_binding" "dev_view" {
   metadata {
