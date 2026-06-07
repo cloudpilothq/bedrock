3743ec29038e249151f426d95adae636b34fcf2c
Victor Omerusure Umukoro <appvictorben@gmail.com>
2026-06-07 19:27:45 +0100

    Scale EKS node group to 3 instances to automatically apply the new max-pods configuration

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -38,8 +38,8 @@ module "eks" {
   eks_managed_node_groups = {
     bedrock_nodes = {
       min_size     = 1
-      max_size     = 2
-      desired_size = 2
+      max_size     = 3
+      desired_size = 3
 
       instance_types = ["t3.micro"]
       capacity_type  = "ON_DEMAND"
